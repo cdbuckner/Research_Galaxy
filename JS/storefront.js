65b9a8c16f6c0f61070e69d5f77da0991616b3a4
Timur <tsadykhov@gmail.com>
2013-07-21 18:08:44 -0400

    Added some media queries.

--- JS/storefront.js
+++ JS/storefront.js
@@ -10,8 +10,8 @@ function onScroll(e) {
     if(window.scrollY >= navOrigOffsetY) { navbar.classList.add('navbar-fixed-top') }
     else { navbar.classList.remove('navbar-fixed-top'); }
 
-    if(window.scrollY >= headerHeight) { header.style.top = "0px"; }
-    else { header.style.top = (scrollY * .05) + "px" }
+    /*if(window.scrollY >= headerHeight) { header.style.top = "0px"; }
+    else { header.style.top = (scrollY * .05) + "px" }*/
 };
 
 $('.open_card_button').on("click", openCard);
